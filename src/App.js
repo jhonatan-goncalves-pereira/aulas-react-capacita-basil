29d3457bb0a6701909648487b0a1ef034a25b104
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2025-01-26 22:19:44 -0300

    trabalhando com roteamento em um menu com exemplo usando recursos do react-router-dom simulando menu Home, Login, Cadastro e AddProduto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 import Button from './components/Button';
 import Contador from './components/Contador';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import AddProduto from './pages/AddProduto';
+
 
 function App() {
   return (
-    <div>
-      <div>
-        <Button label="btnTxt"></Button>
-      </div>
-        
-      <div>
-        <Contador></Contador>
-      </div>
-    </div>
-    
+  <div>
+
+    <nav>
+      <a href="/">Home</a>
+      <a href="/adicionar-produto">Adicionar Produto</a>
+      <a href="/login">Login</a>
+    </nav>
+  
+   <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/adicionar-produto" element={<AddProduto/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/cadastro" element={<Cadastro/>}></Route>
+    </Routes>
+   </Router>
+
+   
+   </div>
   );
 }
 
---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cadastro = () =>{
+    return(   <h2>Cadastre-se</h2> );
+}
+
+export default Cadastro;
